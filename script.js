1e27ec35ae99e0008ab0e3b8997a26195693ea29
Dylan Munro <dylmunro@gmail.com>
2021-11-30 12:47:06 -0500

    Adds navigation bar text

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function loadPage(url) {
   window.location.href = url;
 }
+
+function scrollToPageLocation(elementId) {
+  document.getElementById(elementId).scrollIntoView({behavior: 'smooth'});
+}
